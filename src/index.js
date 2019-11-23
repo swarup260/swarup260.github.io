0c04d89b8e8da67a930c924dac90e2418edad18e
swarup das <swarup260@gmail.com>
2019-11-24 00:56:07 +0530

    udpated design

--- src/index.js
+++
@@ -1,9 +0,0 @@
-
-$(document).ready(function(){
-  $('.sidenav').sidenav();
-});
-
-const copyrighttText = document.querySelector('#copyright');
-const yr = new Date();
-copyrighttText.innerHTML = `Copyright &#169; ${yr.getFullYear()} <span>Swarup Das</span>`
-
