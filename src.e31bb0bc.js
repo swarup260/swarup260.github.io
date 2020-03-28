e504813a364c051fa6f24299cc79b0722a4a0c10
swarup <swarup260@gmail.com>
2020-03-28 16:15:32 +0530

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -120,6 +120,10 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 })({"index.js":[function(require,module,exports) {
 $(document).ready(function () {
   $('.sidenav').sidenav();
+  $('.carousel').carousel({
+    fullWidth: true,
+    indicators: true
+  });
 });
 var copyrighttText = document.querySelector('#copyright');
 var yr = new Date();
@@ -152,7 +156,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51995" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33921" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
